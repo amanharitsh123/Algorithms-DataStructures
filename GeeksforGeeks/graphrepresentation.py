03cb089cee1a62ed87effe0560aa5354be0055ec
Aman Sharma <amanharitsh123@gmail.com>
2019-06-27 18:40:17 +0530

    BFS and DFS added

--- GeeksforGeeks/graphrepresentation.py
+++ GeeksforGeeks/graphrepresentation.py
@@ -1,3 +1,4 @@
+import queue
 # Graph Representation using adjancy list
 # V node and E edges
 class Graph:
@@ -11,9 +12,34 @@ class Graph:
         self.nodes[x].append(y)
     
     def bfs(self,source):
-        pass
-
+        visited = [False for x in range(self.V + 1)]
+        ans = []
+        q = queue.Queue()
+        q.put(source)
+        ans = []
+        while q.qsize() >0:
+            top=q.get()
+            if not visited[top]:
+                for x in self.nodes[top]:
+                    if not visited[x]:
+                        q.put(x)
+                visited[top]=True
+                ans.append(top)
+        return ans
     
+    def dfs_util(self,source,visited,ans):
+        if not visited[source]:
+            visited[source]=True
+            ans.append(source)
+            for x in self.nodes[source]:
+                self.dfs_util(x,visited,ans)
+                
+    def dfs(self,source):
+        visited = [False for x in range(self.V+1)]
+        ans=[]
+        self.dfs_util(source,visited,ans)
+        return ans
+
     def printlist(self):
         print(self.nodes)
 
@@ -27,3 +53,5 @@ graph.addedge(5,6)
 graph.addedge(7,8)
 graph.addedge(1,10)
 graph.printlist()
+print(graph.bfs(1))
+print(graph.dfs(1))
